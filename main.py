4a89307cdb786c8b7460c4727857ed72f6acdf5c
HM <hieumdd@gmail.com>
2021-05-04 09:49:36 +0700

    Async

--- main.py
+++ main.py
@@ -1,65 +1,137 @@
 import os
-import sys
 import json
-import asyncio
+import time
+import base64
 from datetime import datetime, timedelta
+from urllib3.util.retry import Retry
+
 
 import requests
-import aiohttp
-from tqdm import tqdm
+from requests.adapters import HTTPAdapter
 from google.cloud import bigquery
-
-# To run asyncio on Windows
-if sys.platform == "win32":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+import jinja2
 
 
-class FacebookAdsInsightsJob:
-    def __init__(self, ads_account, table, **kwargs):
+class FacebookAdsInsights:
+    def __init__(self, **kwargs):
         """Init. Getting env & configs from envs"""
 
-        self.ads_account = ads_account
-        self.dataset = "Facebook"
-        self.table = table
-
-        self.access_token = os.getenv("ACCESS_TOKEN")
         self.api_ver = os.getenv("API_VER")
+        self.ads_account = os.getenv("ADS_ACCOUNT")
+        self.access_token = os.getenv("ACCESS_TOKEN")
 
-        with open("config.json", "r") as f:
-            config = json.load(f)
-        self.fields = config.get("fields")
-        self.windows = config.get("action_attribution_windows")
-        self.fields_with_windows = config.get("fields_with_windows")
+        self.dataset = "Facebook"
+        self.table = os.getenv("TABLE")
+        self.base_url = f"https://graph.facebook.com/{self.api_ver}/"
 
-        if "end_date" in kwargs:
+        if "start_date" in kwargs and "end_date" in kwargs:
             self.manual = True
-            self.end_date = datetime.strptime(kwargs["end_date"], "%Y-%m-%d")
+            self.start_date = kwargs["start_date"]
+            self.end_date = kwargs["end_date"]
         else:
             self.manual = False
-            self.end_date = datetime.now()
-
-        if "start_date" in kwargs:
-            self.start_date = datetime.strptime(kwargs["start_date"], "%Y-%m-%d")
-        else:
-            self.start_date = datetime.now() - timedelta(days=28)
+            self.end_date = datetime.now().strftime("%Y-%m-%d")
+            self.start_date = (datetime.now() - timedelta(days=28)).strftime("%Y-%m-%d")
 
         self.num_processed = 0
         self.job_ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    def generate_time_ranges(self):
-        """Generate date ranges between two dates
+        config = self._init_config()
+        self.field = config.get("field")
+        self.fields = self.field.get("fields")
+        self.windows = self.field.get("action_attribution_windows")
+        self.fields_with_windows = self.field.get("fields_with_windows")
+        self.key = config.get("key")
 
-        Returns:
-            list: List of dates
-        """
+        self.session = self._init_session()
 
-        date_ranges = [
-            self.start_date + timedelta(n)
-            for n in range(int((self.end_date - self.start_date).days) + 1)
-        ]
-        return list(map(lambda x: x.strftime("%Y-%m-%d"), date_ranges))
+        self.client = bigquery.Client()
 
-    def transform_result(self, result):
+    def _init_config(self):
+        with open(f"configs/config.json", "r") as f:
+            config = json.load(f)
+        return config
+
+    def _init_session(self):
+        session = requests.Session()
+        retry = Retry(
+            total=5,
+            backoff_factor=2,
+            status_forcelist=[100, 413, 429, 503],
+        )
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount("https://", adapter)
+        return session
+
+    def _init_table(self, table):
+        return f"Ads_Insights_{table}"
+
+    def fetch(self):
+        report_run_id = self._fetch_report_id()
+        report_run_id = self._poll_report_id(report_run_id)
+        return self._fetch_insights(report_run_id)
+
+    def _breakdowns_params_factory(self, params):
+        return params
+
+    def _fetch_report_id(self):
+        url = self.base_url + f"{self.ads_account}/insights"
+        time_range = {"since": self.start_date, "until": self.end_date}
+        params = {
+            "access_token": self.access_token,
+            "level": "ad",
+            "time_range": json.dumps(time_range),
+            "time_increment": 1,
+            "fields": json.dumps(self.fields),
+            "action_attribution_windows": json.dumps(self.windows),
+        }
+        params = self._breakdowns_params_factory(params)
+        with self.session.post(url, params=params) as r:
+            res = r.json()
+        return res["report_run_id"]
+
+    def _poll_report_id(self, report_run_id):
+        url = self.base_url + report_run_id
+        while True:
+            params = {"access_token": self.access_token}
+            with self.session.get(url, params=params) as r:
+                res = r.json()
+            if (res["async_status"] == "Job Completed") and (
+                res["async_percent_completion"] == 100
+            ):
+                return report_run_id
+            elif res["async_status"] in ["Job Failed", "Job Skipped"]:
+                raise RuntimeError
+            else:
+                print(res["async_percent_completion"])
+                time.sleep(3)
+
+    def _fetch_insights(self, report_run_id):
+        url = self.base_url + f"{report_run_id}/insights"
+        rows = []
+        after = None
+        while True:
+            params = {"access_token": self.access_token, "limit": 500}
+            if after is not None:
+                params["after"] = after
+            with self.session.get(url, params=params) as r:
+                res = r.json()
+            data = res.get("data")
+            rows.extend(data)
+            next = res.get("paging").get("next")
+            if next is not None:
+                after = res.get("paging").get("cursors").get("after")
+            else:
+                break
+        return rows
+
+    def transform(self, rows):
+        rows = [self._transform_prefix(row) for row in rows]
+        rows = [dict(row, **{"_batched_at": self.job_ts}) for row in rows]
+        self.num_processed = len(rows)
+        return rows
+
+    def _transform_prefix(self, row):
         """Transform fields name to adhere to columns' naming requirements
 
         Args:
@@ -70,72 +142,17 @@ class FacebookAdsInsightsJob:
         """
 
         for nest in self.fields_with_windows:
-            actions = result.get(nest)
-            if actions:
-                for i in actions:
-                    for k in list(
-                        filter(lambda x: x not in ["value", "action_type"], list(i.keys()))
-                    ):
+            fields = row.get(nest)
+            if fields:
+                for i in fields:
+                    for k in [j for j in i.keys() if j not in ["value", "action_type"]]:
                         i["_" + k] = i.pop(k)
-        return result
-
-    async def fetch_one(self, sessions, dt):
-        """Fetch one day of data
-
-        Args:
-            sessions (aiohttp.ClientSession): Client to requests API
-            dt (str): Date in %Y-%m-%d
-        """
-
-        time_range = {"since": dt, "until": dt}
-        params = {
-            "access_token": self.access_token,
-            "level": "ad",
-            "time_range": json.dumps(time_range),
-            "fields": json.dumps(self.fields),
-            "action_attribution_windows": json.dumps(self.windows),
-            "limit": 500,
-        }
-        async with sessions.get(
-            "https://graph.facebook.com/{api_ver}/{ads_account}/insights".format(
-                api_ver=self.api_ver,
-                ads_account=self.ads_account,
-            ),
-            params=params,
-        ) as r:
-            assert r.status == 200
-            response = await r.json()
-        results = response.get("data")
-
-        if results is not None:
-            self.num_processed += 1
-            return list(map(self.transform_result, results))
-        else:
-            return None
-
-    async def fetch_all(self):
-        """Organize fetching all data in specified date ranges"""
-
-        date_ranges = self.generate_time_ranges()
-        async with aiohttp.ClientSession() as sessions:
-            tasks = [
-                asyncio.create_task(self.fetch_one(sessions, dt)) for dt in date_ranges
-            ]
-            rows = [
-                await f for f in tqdm(asyncio.as_completed(tasks), total=len(tasks))
-            ]
-
-        rows = [i for i in rows if i is not None]
-        rows = [i for sublist in rows for i in sublist]
-        return [dict(item, **{"_batched_at": self.job_ts}) for item in rows]
+        return row
 
     def load(self, rows):
-        client = bigquery.Client()
+        schema = self._schema_factory()
 
-        with open("schemas/AdsInsights.json") as f:
-            schema = json.load(f)
-
-        return client.load_table_from_json(
+        return self.client.load_table_from_json(
             rows,
             f"{self.dataset}._stage_{self.table}",
             job_config=bigquery.LoadJobConfig(
@@ -145,48 +162,97 @@ class FacebookAdsInsightsJob:
             ),
         ).result()
 
+    def _schema_factory(self):
+        with open("schemas/AdsInsights.json") as f:
+            schema = json.load(f)
+        return schema
+
+    def update(self):
+        loader = jinja2.FileSystemLoader(searchpath="./")
+        env = jinja2.Environment(loader=loader)
+
+        template = env.get_template("update.sql.j2")
+        rendered_query = template.render(
+            dataset=self.dataset,
+            table=self.table,
+            p_key=",".join(self.key.get("p_key")),
+            incremental_key=self.key.get("incremental_key"),
+            partition_key=self.key.get("partition_key"),
+        )
+
+        _ = self.client.query(rendered_query).result()
+
     def run(self):
         """Main extract & load operation
 
         Returns:
             dict: Response for server
         """
-        try:
-            rows = asyncio.run(self.fetch_all())
-            load_results = self.load(rows)
-        except AssertionError:
-            load_results = None
+        rows = self.fetch()
+        rows = self.transform(rows)
+        results = self.load(rows)
+        self.update()
 
         return {
             "ads_account": self.ads_account,
-            "start_date": self.start_date.strftime("%Y-%m-%d"),
-            "end_date": self.end_date.strftime("%Y-%m-%d"),
+            "start_date": self.start_date,
+            "end_date": self.end_date,
             "num_processed": self.num_processed,
-            "output_rows": getattr(load_results, "output_rows", None),
-            "load_errors": getattr(load_results, "errors", None),
+            "output_rows": getattr(results, "output_rows", None),
+            "errors": getattr(results, "errors", None),
         }
 
 
-def main(request):
-    VuaNemTk01 = FacebookAdsInsightsJob(
-        "act_2419414334994459", "Ads_Insights_VuaNemTk01"
-    )
-    VuaNemUSD = FacebookAdsInsightsJob("act_808142069649310", "Ads_Insights_VuaNemUSD")
+class FacebookAdsInsightsHourly(FacebookAdsInsights):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def _init_table(self):
+        return "AdsInsights_Hourly"
+
+    def _init_config(self):
+        with open(f"configs/config_Hourly.json", "r") as f:
+            config = json.load(f)
+        return config
+
+    def _breakdowns_params_factory(self, params):
+        params["level"] = "account"
+        params["breakdowns"] = "hourly_stats_aggregated_by_advertiser_time_zone"
+        return params
+
+    def _schema_factory(self):
+        with open("schemas/AdsInsights_Hourly.json") as f:
+            schema = json.load(f)
+        return schema
+
+
+def facebook_ads_insights_factory(**kwargs):
+    if "breakdowns" in kwargs:
+        if kwargs["breakdowns"] == "hourly":
+            return FacebookAdsInsightsHourly(**kwargs)
+        else:
+            return FacebookAdsInsights(**kwargs)
+    else:
+        return FacebookAdsInsights(**kwargs)
+
+
+def main(event, context):
+    data = event["data"]
+    message = json.loads(base64.b64decode(data).decode("utf-8"))
+    print(message)
+    if "start_date" in message and "end_date" in message:
+        job = facebook_ads_insights_factory(
+            breakdowns=message.get("breakdowns"),
+            start_date=message["start_date"],
+            end_date=message["end_date"],
+        )
+    else:
+        job = facebook_ads_insights_factory(breakdowns=message.get("breakdowns"))
 
     results = {
         "pipelines": "Facebook Ads Insights",
-        "results": [VuaNemTk01.run(), VuaNemUSD.run()],
+        "results": job.run(),
     }
     print(results)
-
-    _ = requests.post(
-        "https://api.telegram.org/bot{token}/sendMessage".format(
-            token=os.getenv("TELEGRAM_TOKEN")
-        ),
-        json={
-            "chat_id": os.getenv("TELEGRAM_CHAT_ID"),
-            "text": json.dumps(results, indent=4),
-        },
-    )
 
     return results
--- test_units.py
+++ test_units.py
@@ -1,11 +1,49 @@
+import json
+import base64
+from datetime import datetime, timedelta
 from unittest.mock import Mock
 
 from main import main
+from assertions import assertion
 
+mock_context = Mock()
+mock_context.event_id = "617187464135194"
+mock_context.timestamp = "2019-07-15T22:09:03.761Z"
+
+
+def test_manual():
+    message = {
+        "start_date": (datetime.now() - timedelta(days=10)).strftime("%Y-%m-%d"),
+        "end_date": (datetime.now() - timedelta(days=10)).strftime("%Y-%m-%d"),
+    }
+    message_json = json.dumps(message)
+    data = {"data": base64.b64encode(message_json.encode("utf-8"))}
+    res = main(data, mock_context)
+    assertion(res)
 
 def test_auto():
-    data = {}
-    req = Mock(get_json=Mock(return_value=data), args=data)
-    res = main(req)
-    for i in res["results"]:
-        assert i["output_rows"] > 0
+    message = {}
+    message_json = json.dumps(message)
+    data = {"data": base64.b64encode(message_json.encode("utf-8"))}
+    res = main(data, mock_context)
+    assertion(res)
+
+def test_manual_hourly():
+    message = {
+        "breakdowns": "hourly",
+        "start_date": (datetime.now() - timedelta(days=10)).strftime("%Y-%m-%d"),
+        "end_date": (datetime.now() - timedelta(days=10)).strftime("%Y-%m-%d"),
+    }
+    message_json = json.dumps(message)
+    data = {"data": base64.b64encode(message_json.encode("utf-8"))}
+    res = main(data, mock_context)
+    assertion(res)
+
+def test_auto_hourly():
+    message = {
+        "breakdowns": "hourly"
+    }
+    message_json = json.dumps(message)
+    data = {"data": base64.b64encode(message_json.encode("utf-8"))}
+    res = main(data, mock_context)
+    assertion(res)
