d35cfce96c4215d8dcb26fb9f778b41d15294ace
HM <hieumdd@gmail.com>
2022-10-03 10:36:09 +0700

    fix: Reenable accounts

--- tasks/tasks_service.py
+++ tasks/tasks_service.py
@@ -4,10 +4,10 @@ from tasks import cloud_tasks
 ACCOUNTS = [
     # "808142069649310", # "VuaNemUSD
     # "2419414334994459", # "VuaNemTK01
-    # "3921338037921594",  # NovaOn
-    # "408351477456855",  # "DiamondVuaNemLZD
-    # "1082729769139634",  # "DiamondVuaNemShopee
-    # "796051681037501",  # "DiamondVuaNemTiki
+    "3921338037921594",  # NovaOn
+    "408351477456855",  # "DiamondVuaNemLZD
+    "1082729769139634",  # "DiamondVuaNemShopee
+    "796051681037501",  # "DiamondVuaNemTiki
     "732801611166280",  # "Vuanem JSC _ 01
     "4769275693175777",  # "Vuanem JSC _ 02
 ]
